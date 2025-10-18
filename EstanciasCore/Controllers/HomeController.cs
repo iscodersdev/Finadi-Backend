52acbce188446c9749f5f5880631d767512a84b0
jorgeiscoders <jorge.cutulli@iscoders.com.ar>
2025-10-18 05:13:59 -0300

    Resumen Tarjeta

--- EstanciasCore/Controllers/HomeController.cs
+++ EstanciasCore/Controllers/HomeController.cs
@@ -31,7 +31,7 @@ namespace EstanciasCore.Controllers
         }
         public IActionResult Index()
         {
-            _resumen.GenerarResumenTarjetas();
+            //_resumen.GenerarResumenTarjetas();
             AddPageAlerts(PageAlertType.Success, $"Bienvenido {User.Identity.Name}!");        
             var usuario = _context.Usuarios.FirstOrDefault(x => x.Email == User.Identity.Name);
             ViewBag.title1 = "Socios Con App";
